86265caefc943520254654816906d2cc35b1db83
xif10416s <448102337@qq.com>
2015-02-10 12:25:01 +0800

    tttttt

---
+++ zookeeper/src/main/java/org/zookeeper/config/ConfigChangeListener.java
@@ -0,0 +1,11 @@
+package org.zookeeper.config;
+
+/**
+ * 监听器，监听配置的改变
+ * 
+ * @author june
+ * 
+ */
+public abstract interface ConfigChangeListener {
+	public abstract void configChanged(String paramString1, String paramString2);
+}
