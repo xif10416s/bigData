8694e480996cdd876c5493c2966b61045ab53718
xif10416s <448102337@qq.com>
2015-02-10 14:33:25 +0800

    tttt

--- zookeeper/src/main/java/org/zookeeper/config/ZkConfigPublisher.java
+++ zookeeper/src/main/java/org/zookeeper/config/ZkConfigPublisher.java
@@ -59,7 +59,7 @@ public class ZkConfigPublisher {
 		publishConfigs(client, ZK_CONFIG_ROOTNODE, confDir);
 	}
 
-	private static void publishConfigs(ZkClient client, String rootNode,
+	public static void publishConfigs(ZkClient client, String rootNode,
 			File confDir) {
 		File[] confs = confDir.listFiles();
 		int success = 0;
@@ -68,40 +68,50 @@ public class ZkConfigPublisher {
 			if (!conf.isFile()) {
 				continue;
 			}
-			String name = conf.getName();
-			String path = ZkUtils.getZkPath(rootNode, name);
-			ZkUtils.mkPaths(client, path);
-			String content;
-			try {
-				content = FileUtils.readFileToString(conf, "UTF-8");
-			} catch (IOException e) {
-				System.err.println("错误: 读取文件内容时遇到异常:" + e.getMessage());
-				failed++;
-				continue;
-			}
-			if (!client.exists(path)) {
-				try {
-					client.createPersistent(path);
-					client.writeData(path, content);
-				} catch (Throwable e) {
-					System.err.println("错误: 尝试发布配置失败: " + e.getMessage());
-					failed++;
-					continue;
-				}
-				System.out.println("提示: 已经成功将配置文件" + conf + "内容发布为新的ZK配置"
-						+ path);
+			
+			boolean isSucc = publishConfig(client, rootNode, conf);
+			if(isSucc) {
+				success++;
 			} else {
-				try {
-					client.writeData(path, content);
-				} catch (Throwable e) {
-					System.err.println("错误: 尝试发布配置失败: " + e.getMessage());
-					failed++;
-					continue;
-				}
-				System.out.println("提示: 已经成功将配置文件" + conf + "内容更新到ZK配置" + path);
+				failed++;
 			}
-			success++;
+			
 		}
 		System.out.println("提示: 完成配置发布，成功" + success + "，失败" + failed + "。");
+	}
+	
+	public static boolean publishConfig(ZkClient client, String rootNode,
+			File conf) {
+		String name = conf.getName();
+		String path = ZkUtils.getZkPath(rootNode, name);
+		ZkUtils.mkPaths(client, path);
+		String content;
+		try {
+			content = FileUtils.readFileToString(conf, "UTF-8");
+		} catch (IOException e) {
+			System.err.println("错误: 读取文件内容时遇到异常:" + e.getMessage());
+			return false;
+		}
+		if (!client.exists(path)) {
+			try {
+				client.createPersistent(path);
+				client.writeData(path, content);
+			} catch (Throwable e) {
+				System.err.println("错误: 尝试发布配置失败: " + e.getMessage());
+				return false;
+			}
+			System.out.println("提示: 已经成功将配置文件" + conf + "内容发布为新的ZK配置"
+					+ path);
+		} else {
+			try {
+				client.writeData(path, content);
+			} catch (Throwable e) {
+				System.err.println("错误: 尝试发布配置失败: " + e.getMessage());
+				return false;
+			}
+			System.out.println("提示: 已经成功将配置文件" + conf + "内容更新到ZK配置" + path);
+		}
+		
+		return true;
 	}
 }
--- zookeeper/src/test/java/org/zookeeper/config/ReadPropertiesTest.java
+++ zookeeper/src/test/java/org/zookeeper/config/ReadPropertiesTest.java
@@ -1,29 +1,28 @@
 package org.zookeeper.config;
-//package com.embracesource.config;
-//
-//import junit.framework.TestCase;
-//
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-//public class ReadPropertiesTest extends TestCase {
-//	private DynamicPropertiesHelperFactory helperFactory;
-//
-//	protected void setUp() throws Exception {
-//		super.setUp();
-//		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
-//				"test-spring-config.xml");
-//		this.helperFactory = ((DynamicPropertiesHelperFactory) ctx
-//				.getBean(DynamicPropertiesHelperFactory.class));
-//	}
-//
-//	public void testReadProperties() throws InterruptedException {
-//		DynamicPropertiesHelper helper = this.helperFactory
-//				.getHelper("test.properties");
-//
-//		while (true) {
-//			System.out.println(helper.getProperty("a"));
-//			Thread.sleep(5000L);
-//		}
-//
-//	}
-//}
+
+import junit.framework.TestCase;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ReadPropertiesTest extends TestCase {
+	private DynamicPropertiesHelperFactory helperFactory;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"test-spring-config.xml");
+		this.helperFactory = ((DynamicPropertiesHelperFactory) ctx
+				.getBean(DynamicPropertiesHelperFactory.class));
+	}
+
+	public void testReadProperties() throws InterruptedException {
+		DynamicPropertiesHelper helper = this.helperFactory
+				.getHelper("a.properties");
+
+		while (true) {
+			System.out.println(helper.getProperty("a"));
+			Thread.sleep(5000L);
+		}
+
+	}
+}
