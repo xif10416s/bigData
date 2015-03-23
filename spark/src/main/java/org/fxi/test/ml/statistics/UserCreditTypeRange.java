5734d39c49b06c240e635d478996ce5ac146e065
xif10416s <448102337@qq.com>
2015-03-23 18:52:11 +0800

    jjj

--- spark/src/main/java/org/fxi/test/ml/statistics/UserCreditTypeRange.java
+++ spark/src/main/java/org/fxi/test/ml/statistics/UserCreditTypeRange.java
@@ -26,7 +26,7 @@ public class UserCreditTypeRange implements Serializable {
 	public void getTotalCreditRange() {
 		List<RunTask> list = new ArrayList<RunTask>();
 		list.add(new RunTask(
-				"select sum(creditRevenue),sum(creditRemaining) ,sum(recommendCredit) ,sum(downloadCredit) ,sum(signCredit) ,sum(actionCredit)  ,sum(rightCredit)  ,sum(shareCredit) ,sum(activityCredit) from   userCredit   limit 100  ",
+				"select sum(creditRevenue),sum(creditRemaining) ,sum(recommendCredit) ,sum(downloadCredit) ,sum(signCredit) ,sum(actionCredit)  ,sum(rightCredit)  ,sum(shareCredit) ,sum(activityCredit) from   userCredit    ",
 				new ResultHander() {
 
 					@Override
@@ -58,7 +58,7 @@ public class UserCreditTypeRange implements Serializable {
 	public void getObtainTopRightCreditUserInfo() {
 		List<RunTask> list = new ArrayList<RunTask>();
 		list.add(new RunTask(
-				"select sum(creditRevenue),sum(creditRemaining) ,sum(recommendCredit) ,sum(downloadCredit) ,sum(signCredit) ,sum(actionCredit)  ,sum(rightCredit)  ,sum(shareCredit) ,sum(activityCredit) from   userCredit   limit 100  ",
+				"select rightCredit  from   userCredit   limit 100  ",
 				new ResultHander() {
 
 					@Override
@@ -66,7 +66,7 @@ public class UserCreditTypeRange implements Serializable {
 						Object first = schema.first();
 						
 						Utils.writePropertiesFile(
-								"C:/ml/result/getTotalCreditRange.txt",
+								"C:/ml/result/getObtainTopRightCreditUserInfo.txt",
 								"sum(creditRevenue),sum(creditRemaining) ,sum(recommendCredit) ,sum(downloadCredit) ,sum(signCredit) ,sum(actionCredit)  ,sum(rightCredit)  ,sum(shareCredit) ,sum(activityCredit)", first + "");
 						try {
 							Thread.sleep(60000);
@@ -80,6 +80,111 @@ public class UserCreditTypeRange implements Serializable {
 		));
 		
 
+		SqlHelper.executeSql(list, new UserInfoSchemaLoader(),
+				new UserCreditSchemaLoader());
+
+	}
+	
+	@Test
+	public void getRemainCreditRange() {
+		List<RunTask> list = new ArrayList<RunTask>();
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=0 and creditRemaining<500  ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"0<=R<500", first + "");
+					}
+				}
+
+		));
+		
+		
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=500 and creditRemaining<3000  ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"500<=R<3000", first + "");
+					}
+				}
+
+		));
+		
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=3000 and creditRemaining<5000  ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"3000<=R<5000", first + "");
+					}
+				}
+
+		));
+		
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=5000 and creditRemaining<20000  ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"5000<=R<20000", first + "");
+					}
+				}
+
+		));
+
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=20000 and creditRemaining<200000  ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"20000<=R<200000", first + "");
+					}
+				}
+
+		));
+		
+		list.add(new RunTask(
+				"select count(*)  from   userCredit  where creditRemaining>=200000 ",
+				new ResultHander() {
+
+					@Override
+					public void handler(JavaSchemaRDD schema) {
+						Object first = schema.first();
+						
+						Utils.writePropertiesFile(
+								"C:/ml/result/getRemainCreditRange.txt",
+								"200000<=R", first + "");
+					}
+				}
+
+		));
+		
 		SqlHelper.executeSql(list, new UserInfoSchemaLoader(),
 				new UserCreditSchemaLoader());
 
