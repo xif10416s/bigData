f33bb822a3a7ef297eddfd1b25edf9ca076fe3e7
xif10416s <448102337@qq.com>
2015-03-25 09:55:11 +0800

    ttt

--- spark/src/main/java/org/fxi/test/ml/scheams/impl/UserCreditLogSchemaLoader.java
+++ spark/src/main/java/org/fxi/test/ml/scheams/impl/UserCreditLogSchemaLoader.java
@@ -9,6 +9,7 @@ import org.apache.spark.sql.api.java.JavaSQLContext;
 import org.apache.spark.sql.api.java.JavaSchemaRDD;
 import org.fxi.test.ml.bean.UserCredit;
 import org.fxi.test.ml.bean.UserCreditLog;
+import org.fxi.test.ml.bean.UserInfo;
 import org.fxi.test.ml.scheams.SchemaLoader;
 import org.fxi.test.ml.util.Utils;
 
@@ -16,33 +17,45 @@ public class UserCreditLogSchemaLoader implements SchemaLoader, Serializable {
 
 	@Override
 	public void loadSchema(JavaSparkContext ctx, JavaSQLContext sqlCtx) {
-		System.out.println("=== Data source: UserCreditLogSchemaLoader RDD ===");
+		System.out
+				.println("=== Data source: UserCreditLogSchemaLoader RDD ===");
 		// Load a text file and convert each line to a Java Bean.
-		JavaRDD<UserCreditLog> userCredit = ctx.textFile(
-				"C:/Users/fxi/Documents/20150319/20150319/hq_user_credit_log_2015_03_19.csv").map(
-				new Function<String, UserCreditLog>() {
+		JavaRDD<UserCreditLog> userCreditLog = ctx
+				.textFile(
+						"C:/Users/fxi/Documents/20150319/20150319/hq_user_credit_log_2015_03_19.csv")
+				.map(new Function<String, UserCreditLog>() {
 					@Override
 					public UserCreditLog call(String line) {
 						String[] parts = line.split("	");
 						UserCreditLog userCreditLog = new UserCreditLog();
-						
-						if(parts.length >19 && parts[18].length() == 32) {
+
+						if (parts.length > 19 && parts[18].length() == 32) {
 							userCreditLog.setUserId(parts[18]);
 							userCreditLog.setLogTime(Long.parseLong(parts[16]));
 							userCreditLog.setCredit(Utils.valueOf(parts[9]));
-							userCreditLog.setCommodityType(Utils.valueOf(parts[7]));
+							userCreditLog.setCommodityType(Utils
+									.valueOf(parts[7]));
 							userCreditLog.setChargePhone(parts[20]);
 							userCreditLog.setDrawRealName(parts[21]);
 							userCreditLog.setDrawAccount(parts[19]);
-							
+
 						}
-						
+
 						return userCreditLog;
 					}
 				});
+		JavaRDD<UserCreditLog> filteruserCreditLog = userCreditLog
+				.filter(new Function<UserCreditLog, Boolean>() {
 
+					private static final long serialVersionUID = 145805894691203300L;
+
+					@Override
+					public Boolean call(UserCreditLog v1) throws Exception {
+						return v1.getUserId() != null;
+					}
+				});
 		// Apply a schema to an RDD of Java Beans and register it as a table.
-		JavaSchemaRDD schemaPeople = sqlCtx.applySchema(userCredit,
+		JavaSchemaRDD schemaPeople = sqlCtx.applySchema(filteruserCreditLog,
 				UserCredit.class);
 		schemaPeople.registerTempTable("userCreditLog");
 	}
