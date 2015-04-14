c53e245c3861939611be2ece9c596f125f73411f
xif10416s <448102337@qq.com>
2015-04-14 19:14:19 +0800

    tttt

--- spark/src/main/java/org/apache/spark/examples/cassandra/SparkCassandra.java
+++ spark/src/main/java/org/apache/spark/examples/cassandra/SparkCassandra.java
@@ -17,11 +17,13 @@ public class SparkCassandra {
 				.set("spark.cassandra.connection.host", "122.144.134.67");
 		sparkConf.set("spark.cassandra.auth.username", "hqdb0");
 		sparkConf.set("spark.cassandra.auth.password", "00huaQianV2!");
-		sparkConf.set("spark.cassandra.keyspace","test");
+		sparkConf.set("spark.cassandra.keyspace","mltp").setMaster("local[6]")
+		.setAppName("JavaSparkSQL").set("spark.executor.memory", "11g");;
 		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
 		JavaCassandraSQLContext cc = new JavaCassandraSQLContext(jsc);
 		
-		JavaSchemaRDD sql = cc.sql("SELECT * from test.kv WHERE 1=1 ;");
+		JavaSchemaRDD sql = cc.sql("select ad_id from mltp_ad_action_log_test where action_type =1");
+		
 		List<String> teenagerNames = sql.map(new Function<Row, String>() {
 			@Override
 			public String call(Row row) {
