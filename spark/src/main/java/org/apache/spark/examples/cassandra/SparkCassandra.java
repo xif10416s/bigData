ea7bb5f8d2f8e5745825885251222ffcfd7f3928
xif10416s <448102337@qq.com>
2015-04-15 11:58:11 +0800

    tt

--- spark/src/main/java/org/apache/spark/examples/cassandra/SparkCassandra.java
+++ spark/src/main/java/org/apache/spark/examples/cassandra/SparkCassandra.java
@@ -1,37 +1,62 @@
 package org.apache.spark.examples.cassandra;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.sql.SchemaRDD;
-import org.apache.spark.sql.api.java.JavaSchemaRDD;
-import org.apache.spark.sql.api.java.Row;
-import org.apache.spark.sql.cassandra.CassandraSQLContext;
-import org.apache.spark.sql.cassandra.api.java.JavaCassandraSQLContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.fxi.spark.analytics.learn.bean.ArtistAliasBean;
+
+import com.datastax.spark.connector.japi.CassandraJavaUtil;
+import com.datastax.spark.connector.japi.CassandraRow;
+import com.datastax.spark.connector.japi.SparkContextJavaFunctions;
+import com.datastax.spark.connector.japi.rdd.CassandraJavaRDD;
 
 public class SparkCassandra {
 	public static void main(String[] args) throws Exception {
 		SparkConf sparkConf = new SparkConf(true).setAppName("SparkCassandra")
 				.set("spark.cassandra.connection.host", "122.144.134.67");
-		sparkConf.set("spark.cassandra.auth.username", "hqdb0");
-		sparkConf.set("spark.cassandra.auth.password", "00huaQianV2!");
-		sparkConf.set("spark.cassandra.keyspace","mltp").setMaster("local[6]")
-		.setAppName("JavaSparkSQL").set("spark.executor.memory", "11g");;
+		sparkConf.set("spark.cassandra.keyspace", "mltp").setMaster("local[6]")
+				.setAppName("JavaSparkSQL").set("spark.executor.memory", "11g");
+		;
 		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-		JavaCassandraSQLContext cc = new JavaCassandraSQLContext(jsc);
-		
-		JavaSchemaRDD sql = cc.sql("select ad_id from mltp_ad_action_log_test where action_type =1");
-		
-		List<String> teenagerNames = sql.map(new Function<Row, String>() {
+//		CassandraConnector connector = CassandraConnector.apply(jsc.getConf());
+//		Session session = connector.openSession();
+//		session.execute("DROP KEYSPACE IF EXISTS java_api");
+//		session.execute("CREATE KEYSPACE java_api WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+//		session.execute("CREATE TABLE java_api.products (id INT PRIMARY KEY, name TEXT, parents LIST<INT>)");
+//		session.execute("CREATE TABLE java_api.sales (id UUID PRIMARY KEY, product INT, price DECIMAL)");
+//		session.execute("CREATE TABLE java_api.summaries (product INT PRIMARY KEY, summary DECIMAL)");
+		SparkContextJavaFunctions javaFunctions = CassandraJavaUtil.javaFunctions(jsc);
+		javaFunctions.cassandraTable(arg0, arg1, arg2, arg3)
+		CassandraJavaRDD<CassandraRow> cassandraTable = javaFunctions.cassandraTable("mltp", "mltp_ad_action_log");
+		JavaRDD<Integer> flatMap = cassandraTable.flatMap(new FlatMapFunction<CassandraRow, Integer>() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -2235195728960646682L;
+
 			@Override
-			public String call(Row row) {
-				return "Name: " + row.getString(0);
+			public Iterable<Integer> call(CassandraRow t) throws Exception {
+				Integer day = t.getInt(1);
+				List<Integer> arrayList = new ArrayList<Integer>();
+				if(day == 20150415) {
+					arrayList.add(t.getInt(0));
+				}
+				
+				return arrayList;
 			}
-		}).collect();
-		for (String name : teenagerNames) {
-			System.out.println(name);
+		});
+		
+		List<Integer> collect2 = flatMap.collect();
+		for(int adId : collect2) {
+			System.out.println(adId);
+		}
+		List<CassandraRow> collect = cassandraTable.collect();
+		for(CassandraRow c : collect) {
+			System.out.println(c.getString(0));
 		}
 	}
 }
