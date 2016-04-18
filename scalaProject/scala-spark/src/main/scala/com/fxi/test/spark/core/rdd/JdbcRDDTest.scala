4388adc8c3d343c8e605cd7c2d47a709a905c86e
fxi@huadongmedia.com <12345678>
2016-04-18 20:26:46 +0800

    1111

---
+++ scalaProject/scala-spark/src/main/scala/com/fxi/test/spark/core/rdd/JdbcRDDTest.scala
@@ -0,0 +1,31 @@
+package com.fxi.test.spark.core.rdd
+
+import java.sql.ResultSet
+
+import com.fxi.test.spark.common.utils.JdbcConnectionFactory
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.rdd.JdbcRDD
+
+/**
+  * Created by xifei on 16-4-18.
+  */
+object JdbcRDDTest {
+  val url = "jdbc:mysql://122.144.134.67:3306/servicedb"
+  val user = "huaqianv3"
+  val pass = "huaqianv3"
+
+  def main(args: Array[String]): Unit = {
+    import org.apache.log4j.{Level, Logger}
+    val logLevel = Level.ERROR
+
+    Logger.getLogger("org").setLevel(logLevel)
+    val sparkConf = new SparkConf().setAppName("UnionTest").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+    val cnt = new JdbcRDD[Long](sc, () => JdbcConnectionFactory.getConnectionFactory(url, user, pass).getConnection,
+      "select id  from hq_allocate_ad_log  WHERE id >= ? AND id <= ?", 1460938817747001101L, 1460969380500000101L, 10, (r: ResultSet) => {
+        r.getLong("id")
+      }).count()
+
+    println("=======>"+cnt)
+  }
+}
