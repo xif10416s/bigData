433e9e8ea9728ea5817e1f3026dc093c071f1f3d
席斐 <seki@xifeideMacBook-Pro.local>
2016-04-23 17:34:54 +0800

    111

--- scalaProject/scala-spark/src/main/scala/com/fxi/test/scala/base/BaseTest.scala
+++ scalaProject/scala-spark/src/main/scala/com/fxi/test/scala/base/BaseTest.scala
@@ -80,4 +80,88 @@ class BaseTest {
     inc1(10)
     inc9999(10)
   }
+
+  /**
+    * curring ==> 函数柯理化
+    * 变成两个函数相继调用
+    */
+  @Test
+  def testCurrying() = {
+    def plainOldSum(x: Int, y: Int) = x + y
+    println(plainOldSum(1, 3))
+
+    def curriedSum(x: Int)(y: Int) = x + y
+    println(curriedSum(1)(3))
+
+    //===>定义了一个函数first 函数体又是一个函数
+    def first(x: Int) = (y: Int) => x + y
+    println(first(1)(3)) //相当于调用两次函数,第一次first(1) 返回一个函数 在调用 (3)
+  //单个括号的时候可以用 花括号
+    println(first {
+      1
+    } {
+      3
+    })
+
+    val second = curriedSum(1) _;
+    println(second(3))
+
+    //定义一个函数twice,参数是一个叫op 输入输出都是double的函数,和一个x的double, 函数体是 执行两次op操作
+    def twice(op: Double => Double, x: Double) = op(op(x))
+    println(twice(_ + 1, 5))
+  }
+
+  /**
+    * control abstractions
+    */
+  @Test
+  def testControlAbstraction() = {
+    //一个参数的时候可以用 花括号 代替 括号
+    println("Hello, world!")
+    println {
+      "Hello, world!"
+    }
+  }
+
+  /**
+    * 空参数列表
+    * => 函数体
+    */
+  @Test
+  def testByNameParameter() = {
+    var assertionsEnabled = true
+    def myAssert(predicate: () => Boolean) =
+      if (assertionsEnabled && !predicate())
+        throw new AssertionError
+//    myAssert(()=>3>5)
+
+    //==> “() => Boolean”   --> “=> Boolean”
+
+    def byNameAssert(predicate: => Boolean) =
+      if (assertionsEnabled && !predicate)
+        throw new AssertionError
+
+    // 和 直接定义参数区别
+    // predicate: Boolean <== 定义一个变量, 调用方法前已经计算
+    // predicate: => Boolean  <== 定义了一个函数,没有参数,计算的时候才计算
+    def boolAssert(predicate: Boolean) =
+      if (assertionsEnabled && !predicate)
+        throw new AssertionError
+
+    byNameAssert(10/0 ==0)
+    boolAssert(10/10 == 0)
+    println("")
+  }
+
+  @Test
+  def testImplicitParam() = {
+    implicit val s = "aaa";
+    def f(a:Int)(implicit s:String): Unit = {
+      println(s)
+      println(a)
+    }
+
+    f(1)
+  }
+
 }
