6dbad132afc56ce139d0c31d6d991e51a28948df
fxi@huadongmedia.com <12345678>
2016-04-27 20:29:21 +0800

    1111

--- scalaProject/scala-spark/src/main/scala/com/fxi/test/spark/core/rdd/RDDTest.scala
+++ scalaProject/scala-spark/src/main/scala/com/fxi/test/spark/core/rdd/RDDTest.scala
@@ -1,7 +1,7 @@
 package com.fxi.test.spark.core.rdd
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.Test
 
 /**
@@ -13,7 +13,25 @@ class RDDTest {
 
   @Test
   def testCount(): Unit = {
-    val rdd1 : RDD[Int] =  sc.makeRDD(1 to 1000, 10)
+    val rdd1: RDD[Int] = sc.makeRDD(1 to 1000, 10)
     println(rdd1.count())
+  }
+
+  @Test
+  def testForEachPartition(): Unit = {
+    val rs = sc.makeRDD(1 to 1000, 10).mapPartitions(f => {
+      f.map[Int](i => {
+        1000 + i
+      }).filter(f => f % 2 == 0)
+    })
+
+    println(rs.collect().mkString(","))
+  }
+
+  @Test
+  def testFileToRDD(): Unit = {
+    val userArray = for (line <- scala.io.Source.fromFile("./data/test/userAndAd").getLines()) yield line
+    sc.parallelize(userArray.toSeq).foreach(println(_))
+
   }
 }
