866088ec51c73eb1db58bb5109bba678c3c0a201
席斐 <seki@xifeideMacBook-Pro.local>
2016-05-08 12:47:59 +0800

    11清清浅浅

--- scalaProject/scala-spark/src/main/scala/com/fxi/test/spark/mllib/classification/BinaryClassification.scala
+++ scalaProject/scala-spark/src/main/scala/com/fxi/test/spark/mllib/classification/BinaryClassification.scala
@@ -126,8 +126,48 @@ object BinaryClassification {
 
       val metrics = new BinaryClassificationMetrics(predictionAndLabel)
 
-      println(s"Test areaUnderPR = ${metrics.areaUnderPR()}.")
-      println(s"Test areaUnderROC = ${metrics.areaUnderROC()}.")
+
+
+      // Precision by threshold
+      val precision = metrics.precisionByThreshold
+      precision.foreach { case (t, p) =>
+        println(s"Threshold: $t, Precision: $p")
+      }
+
+      // Recall by threshold
+      val recall = metrics.recallByThreshold
+      recall.foreach { case (t, r) =>
+        println(s"Threshold: $t, Recall: $r")
+      }
+
+      // Precision-Recall Curve
+      val PRC = metrics.pr
+
+      // F-measure
+      val f1Score = metrics.fMeasureByThreshold
+      f1Score.foreach { case (t, f) =>
+        println(s"Threshold: $t, F-score: $f, Beta = 1")
+      }
+
+      val beta = 0.5
+      val fScore = metrics.fMeasureByThreshold(beta)
+      f1Score.foreach { case (t, f) =>
+        println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+      }
+
+      // AUPRC
+      val auPRC = metrics.areaUnderPR
+      println("Area under precision-recall curve = " + auPRC)
+
+      // Compute thresholds used in ROC and PR curves
+      val thresholds = precision.map(_._1)
+
+      // ROC Curve
+      val roc = metrics.roc
+
+      // AUROC
+      val auROC = metrics.areaUnderROC
+      println("Area under ROC = " + auROC)
 
       sc.stop()
     }
