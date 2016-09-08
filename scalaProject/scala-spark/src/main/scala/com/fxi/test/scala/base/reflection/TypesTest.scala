bfc08b6fa1855b41ca474b374bf2020265cfbaee
fxi@huadongmedia.com <12345678>
2016-09-08 18:56:45 +0800

    1111

--- scalaProject/scala-spark/src/main/scala/com/fxi/test/scala/base/reflection/TypesTest.scala
+++ scalaProject/scala-spark/src/main/scala/com/fxi/test/scala/base/reflection/TypesTest.scala
@@ -10,5 +10,31 @@ object TypesTest {
     import scala.reflect.runtime.universe._
     val unit: JavaUniverse#Type = typeOf[List[Int]]
     println(unit)
+
+    println( typeOf[Parent] <:< typeOf[Child])
+    println( typeOf[Child] <:< typeOf[Parent])
+
+    println(typeOf[Double] weak_<:< typeOf[Int])
+
+    println(typeOf[Int] <:< typeOf[Double])
+    println(typeOf[Int] weak_<:< typeOf[Double])
+
+    val p1= new Parent()
+    val p2 =new Parent()
+    def getType[T: TypeTag](obj: T) = typeOf[T]
+
+    println(getType(p1) =:= getType(p2))
+    println(getType(p1).members)
+    println(getType(p1).declarations)
+
+    println(typeOf[List[_]].member("map": TermName))
+
+    println(typeOf[List[_]].member("S1elf": TypeName))
+
+    typeOf[List[Int]].members.filter(_.isPrivate).foreach(println _)
   }
 }
+
+
+class Parent
+class Child extends Parent
