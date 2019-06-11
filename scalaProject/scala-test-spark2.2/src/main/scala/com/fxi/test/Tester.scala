f33ec509d6b72bc42967ad628b22d2eed2da0933
xifei <xifei198675@gmail.com>
2019-06-11 15:03:10 +0800

    时间序列模型测试

--- scalaProject/scala-test-spark2.2/src/main/scala/com/fxi/test/Tester.scala
+++ scalaProject/scala-test-spark2.2/src/main/scala/com/fxi/test/Tester.scala
@@ -1,13 +1,22 @@
 package com.fxi.test
 
-import org.junit.Test
+import org.apache.spark.sql.SparkSession
+
 
 /**
   * Created by seki on 17/9/16.
   */
-class Tester {
-    @Test
-    def test(): Unit = {
-        println("111")
-    }
+object Tester {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("Spark Pi")
+      .master("local[*]")
+//      .config("spark.jars", "/Users/seki/git/learn/bigData/scalaProject/scala-test-spark2.2/target/scala-test-spark2.2-1.0-SNAPSHOT.jar")
+      .getOrCreate()
+    val count = spark.sparkContext.parallelize(Seq(0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3), 4)
+      .groupBy(f => f)
+    count.collect().foreach(println _)
+  }
+
 }
